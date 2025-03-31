12afb9a7d9aabd9a852a6cbeed22332f7ca85637
Starwolf16 <jmtarrant00@gmail.com>
2025-03-30 23:02:07 -0400

    Update Grass.py
    Remove unnecessary else

--- New Game/Grass.py
+++ New Game/Grass.py
@@ -2,7 +2,6 @@ while True:
     for i in range(get_world_size()):
         if can_harvest():
             harvest()
-        else: 
-            pass
+            
         move(North)
     move(East)
