f7adfec0187adfdbcd5043eb674f92d12d1b0ecb
Starwolf16 <jmtarrant00@gmail.com>
2025-06-14 08:53:34 -0400

    Comments
    Added comments describing what the code does

--- New Game/Carrots.py
+++ New Game/Carrots.py
@@ -1,20 +1,27 @@
+# Harvests, tills, plants, and waters carrots across the world as needed.
 def carrots(): 
     for i in range(get_world_size()):
+
+        # If ready to harvest and soil is correct, harvest and replant
         if can_harvest() and get_ground_type() == Grounds.Soil:
             harvest()
             plant(Entities.Carrot)
-        
+
+        # If ready to harvest but soil is wrong, harvest, till, and replant
         elif can_harvest() and get_ground_type() != Grounds.Soil:
             harvest()
             till()
             plant(Entities.Carrot)
-        
+
+        # If not ready to harvest and soil is wrong, just till and plant
         elif get_ground_type() != Grounds.Soil:
             till()
             plant(Entities.Carrot)
 
 
+        # Water if soil is too dry
         if get_water() < 0.5:
             use_item(Items.Water)
+
         move(North)
     move(East)
