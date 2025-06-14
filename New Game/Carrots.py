49ec56474a280a78d2656d34c99d5f7b91f5abd4
Starwolf16 <jmtarrant00@gmail.com>
2025-06-14 08:42:38 -0400

    Add Harvesting Process
    Added the harvesting process for carrots, including the ability to water the ground and detect if the ground is harvestable and the correct type

--- New Game/Carrots.py
+++ New Game/Carrots.py
@@ -1,8 +1,21 @@
 while True:
     
     for i in range(get_world_size()):
-        if get_ground_type() == Grounds.Grassland:
+        if can_harvest() and get_ground_type() == Grounds.Soil:
+            harvest()
+            plant(Entities.Carrot)
+        
+        elif can_harvest() and get_ground_type() != Grounds.Soil:
+            harvest()
             till()
             plant(Entities.Carrot)
+        
+        elif get_ground_type() != Grounds.Soil:
+            till()
+            plant(Entities.Carrot)
+            
+
+        if get_water() < 0.5:
+            use_item(Items.Water)
         move(North)
     move(East)
