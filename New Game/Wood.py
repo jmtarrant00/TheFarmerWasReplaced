6c62515780ef0a0351ef104574dd55c6f42e1b0a
Starwolf16 <jmtarrant00@gmail.com>
2025-06-15 09:57:07 -0400

    Add Trees
    Added the use of trees along with the bushes

--- New Game/Wood.py
+++ New Game/Wood.py
@@ -1,8 +1,20 @@
 def wood():
+
     for i in range(get_world_size()):
-        if can_harvest():
+        x = get_pos_x()
+        y = get_pos_y()
+
+        if y % 2 == 0 and x % 2 == 0 and can_harvest():
             harvest()
-            plant(Entities.Bush)
+            plant(Entities.Tree)
+        elif y % 2 != 0 and x % 2 != 0 and can_harvest():
+            harvest()
+            plant(Entities.Tree)
+        else:
+            if can_harvest(): 
+                harvest()
+                plant(Entities.Bush)
+        
         
         move(North)
     move(East)
