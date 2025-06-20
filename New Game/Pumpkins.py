6c20a0e189fbdea22f6eda46b8fb44f0c097588e
Starwolf16 <jmtarrant00@gmail.com>
2025-06-19 22:36:24 -0400

    Check for Costs
    Added functionality to check for the costs of pumpkins

--- New Game/Pumpkins.py
+++ New Game/Pumpkins.py
@@ -1,27 +1,52 @@
-def pumpkins():
-    for i in range(get_world_size()):
+from Utils import *
+from Carrots import carrots
 
-        if can_harvest() and get_ground_type() == Grounds.Soil:
-            harvest()
-            plant(Entities.Pumpkin)
+def pumpkins(world_size):
 
-        elif can_harvest() and get_ground_type() != Grounds.Soil:
-            harvest()
-            till()
-            plant(Entities.Pumpkin)
+    while num_items(Items.Carrot) < (7 * (world_size ** 2)):
+        carrots(world_size)
 
-        elif get_ground_type() != Grounds.Soil:
-            till()
-            plant(Entities.Pumpkin)
-            
-        else:
-            plant(Entities.Pumpkin)
-            
-
-        if get_water() < 0.5:
-            while get_water() != 1:
-                use_item(Items.Water)
-
+    for i in range(2):
+        replant_list = []
         
-        move(North)
-    move(East)
+        for x in range(world_size):
+            for y in range(world_size):
+
+                if get_ground_type() != Grounds.Soil:
+                    till()
+
+                if i == 0 and can_harvest():
+                    harvest_process(Entities.Pumpkin)
+
+                elif i == 1 and can_harvest():
+                    pass
+
+                else:
+                    plant(Entities.Pumpkin)
+                    replant_list.append([x, y])
+
+                if get_water() < 0.5:
+                    water()
+                
+                move(North)
+            move(East)
+
+    quick_print(replant_list)
+
+    while len(replant_list) > 0:
+        quick_print(replant_list)
+        for tile in replant_list:
+            quick_print("Moving to: ", tile)
+            moveTo(tile)
+
+            if can_harvest():
+                replant_list.remove(tile)
+                continue
+            else:
+                plant(Entities.Pumpkin)
+    
+    harvest()
+    moveTo([0,0])
+
+
+#pumpkins(get_world_size())
