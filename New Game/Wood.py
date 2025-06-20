c240e9555b69e36653c495379e221af2f57145c7
Starwolf16 <jmtarrant00@gmail.com>
2025-06-19 22:39:47 -0400

    Update Wood.py

--- New Game/Wood.py
+++ New Game/Wood.py
@@ -1,20 +1,23 @@
-def wood():
+def wood(world_size):  
 
-    for i in range(get_world_size()):
-        x = get_pos_x()
-        y = get_pos_y()
+    for y in range(world_size):
+        for x in range(world_size):
+            x_pos = get_pos_x()
+            y_pos = get_pos_y()
 
-        if y % 2 == 0 and x % 2 == 0 and can_harvest():
-            harvest()
-            plant(Entities.Tree)
-        elif y % 2 != 0 and x % 2 != 0 and can_harvest():
-            harvest()
-            plant(Entities.Tree)
-        else:
-            if can_harvest(): 
+            if y_pos % 2 == 0 and x_pos % 2 == 0 and can_harvest():
+                harvest()
+                plant(Entities.Tree)
+            elif y_pos % 2 != 0 and x_pos % 2 != 0 and can_harvest():
+                harvest()
+                plant(Entities.Tree)
+            elif can_harvest(): 
                 harvest()
                 plant(Entities.Bush)
+                        
+            
+            move(North)
+        move(East)
         
         
-        move(North)
-    move(East)
+#wood(get_world_size())
