0d51828a2f05660c6a73b1faca420bbe35665d89
Starwolf16 <jmtarrant00@gmail.com>
2025-06-19 22:34:45 -0400

    Check Ground Type
    Added the ability to check the type of soil the ground currently is

--- New Game/Grass.py
+++ New Game/Grass.py
@@ -1,7 +1,13 @@
-def grass():
-    for i in range(get_world_size()):
-        if can_harvest():
-            harvest()
-            
-        move(North)
-    move(East)
+def grass(world_size):
+    for y in range(world_size):
+        for x in range(world_size):
+            if get_ground_type() != Grounds.Grassland:
+                harvest()
+                till()
+            if can_harvest():
+                harvest()
+                
+            move(North)
+        move(East)
+#while True:
+#    grass(get_world_size())
